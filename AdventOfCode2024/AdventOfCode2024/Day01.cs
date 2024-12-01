93a7186d42bcf5564a2ba0112eee86d7353f2d21
Daniel-Rosbak-Student <drosba22@student.aau.dk>
2024-12-01 13:20:25 +0100

    Day 1 and changed schematic for days

--- AdventOfCode2024/AdventOfCode2024/Day01.cs
+++ AdventOfCode2024/AdventOfCode2024/Day01.cs
@@ -4,8 +4,83 @@ public class Day01 : Day
 {
     public override string Run()
     {
-        string res = "";
+        return PartTwo();
+    }
+
+    private string PartOne()
+    {
+        Console.WriteLine("input: ");
+        List<string> input = new List<string>();
+        string next = Console.ReadLine();
+        while (next != String.Empty)
+        {
+            input.Add(next);
+            next = Console.ReadLine();
+        }
+
+        List<int> first = new List<int>();
+        List<int> second = new List<int>();
+
+        foreach (string line in input)
+        {
+            first.Add(int.Parse(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0]));
+            second.Add(int.Parse(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]));
+        }
+
+
+        int res = 0;
+        first.Sort();
+        second.Sort();
+
+        for (int i = 0; i < first.Count; i++)
+        {
+            res += Math.Abs(first[i] - second[i]);
+        }
         
-        return res;
+        return res.ToString();
+    }
+
+    private string PartTwo()
+    {
+        Console.WriteLine("input: ");
+        List<string> input = new List<string>();
+        string next = Console.ReadLine();
+        while (next != String.Empty)
+        {
+            input.Add(next);
+            next = Console.ReadLine();
+        }
+
+        List<int> first = new List<int>();
+        List<int> second = new List<int>();
+
+        foreach (string line in input)
+        {
+            first.Add(int.Parse(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0]));
+            second.Add(int.Parse(line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]));
+        }
+
+
+        int res = 0;
+        first.Sort();
+        second.Sort();
+
+        for (int i = 0; i < first.Count; i++)
+        {
+            int num = first[i];
+            int mul = 0;
+            for (int j = 0; j < second.Count; j++)
+            {
+                if (second[j] == num)
+                {
+                    mul++;
+                }
+            }
+            Console.WriteLine("#" + first[i]);
+            Console.WriteLine("*" + mul);
+            res += first[i] * mul;
+        }
+        
+        return res.ToString();
     }
 }
