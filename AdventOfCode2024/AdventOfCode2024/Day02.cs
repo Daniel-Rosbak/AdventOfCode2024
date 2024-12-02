136faa2ba8e173f07bee5321cfd92bbb443efd51
Daniel-Rosbak <drosba22@student.aau.dk>
2024-12-02 08:42:00 +0100

    Completed day 2

--- AdventOfCode2024/AdventOfCode2024/Day02.cs
+++ AdventOfCode2024/AdventOfCode2024/Day02.cs
@@ -4,7 +4,7 @@ public class Day02 : Day
 {
     public override string Run()
     {
-        return PartOne();
+        return PartTwo();
     }
     
     private string PartOne()
@@ -18,8 +18,45 @@ public class Day02 : Day
             next = Console.ReadLine();
         }
 
+        List<List<int>> reports = new List<List<int>>();
+
+        foreach (string report in input)
+        {
+            string[] levels = report.Split(" ");
+            List<int> reportList = new List<int>();
+            reports.Add(reportList);
+            foreach (string level in levels)
+            {
+                reportList.Add(int.Parse(level));
+            }
+        }
         int res = 0;
-        
+
+        foreach (List<int> report in reports)
+        {
+            bool safe = true;
+            bool descending = report[0] > report[1];
+            
+            for (int i = 0; i < report.Count - 1; i++)
+            {
+                int change = report[i] - report[i + 1];
+                if (change < -3 || change > 3 || change == 0)
+                {
+                    safe = false;
+                    break;
+                }
+                if (descending && change < 0 || !descending && change > 0)
+                {
+                    safe = false;
+                    break;
+                }
+            }
+
+            if (safe)
+            {
+                res++;
+            }
+        }
         
         return res.ToString();
     }
@@ -34,9 +71,85 @@ public class Day02 : Day
             input.Add(next);
             next = Console.ReadLine();
         }
+        
+        List<List<int>> reports = new List<List<int>>();
+
+        foreach (string report in input)
+        {
+            string[] levels = report.Split(" ");
+            List<int> reportList = new List<int>();
+            reports.Add(reportList);
+            foreach (string level in levels)
+            {
+                reportList.Add(int.Parse(level));
+            }
+        }
 
         int res = 0;
-        
+
+        foreach (List<int> report in reports)
+        {
+            bool safe = true;
+            bool safetyDampened = false;
+            int overallChange = 0;
+            for (int i = 0; i < report.Count - 1; i++)
+            {
+                overallChange += report[i + 1] - report[i];
+            }
+            
+            for (int i = 0; i < report.Count - 1; i++)
+            {
+                int change = report[i + 1] - report[i];
+                
+                if (change < -3 || change > 3 || change == 0)
+                {
+                    safe = false;
+                }
+                if (overallChange > 0 && change < 0 || overallChange < 0 && change > 0)
+                {
+                    safe = false;
+                }
+
+                if (!safe && !safetyDampened)
+                {
+                    safetyDampened = true;
+                    safe = true;
+                    
+                    if (i == 0)
+                    {
+                        change = report[i + 2] - report[i + 1];
+                        if (change < -3 || change > 3 || change == 0)
+                        {
+                            safe = false;
+                        }
+                        if (overallChange > 0 && change < 0 || overallChange < 0 && change > 0)
+                        {
+                            safe = false;
+                        }
+
+                        if (safe)
+                        {
+                            report.RemoveAt(i);
+                        }
+                        else
+                        {
+                            report.RemoveAt(i + 1);
+                        }
+                    }
+                    else
+                    {
+                        report.RemoveAt(i + 1);
+                    }
+                    i--;
+                    
+                }
+            }
+
+            if (safe)
+            {
+                res++;
+            }
+        }
         
         return res.ToString();
     }
