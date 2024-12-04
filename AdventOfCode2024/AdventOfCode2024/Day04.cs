b526927d89eecc031e126bc7b00d9cfdb42f201b
Daniel-Rosbak-Student <drosba22@student.aau.dk>
2024-12-04 07:56:54 +0100

    completed day 4

--- AdventOfCode2024/AdventOfCode2024/Day04.cs
+++ AdventOfCode2024/AdventOfCode2024/Day04.cs
@@ -4,7 +4,7 @@ public class Day04 : Day
 {
     public override string Run()
     {
-        return PartOne();
+        return PartTwo();
     }
     
     private string PartOne()
@@ -19,7 +19,139 @@ public class Day04 : Day
         }
 
         int res = 0;
-        
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            for (int j = 0; j < input[i].Length - 3; j++)
+            {
+                if (input[i][j] == 'X')
+                {
+                    if (input[i][j + 1] == 'M')
+                    {
+                        if (input[i][j + 2] == 'A')
+                        {
+                            if (input[i][j + 3] == 'S')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+
+                if (input[i][j] == 'S')
+                {
+                    if (input[i][j + 1] == 'A')
+                    {
+                        if (input[i][j + 2] == 'M')
+                        {
+                            if (input[i][j + 3] == 'X')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < input.Count - 3; i++)
+        {
+            for (int j = 0; j < input[i].Length; j++)
+            {
+                if (input[i][j] == 'X')
+                {
+                    if (input[i + 1][j] == 'M')
+                    {
+                        if (input[i + 2][j] == 'A')
+                        {
+                            if (input[i + 3][j] == 'S')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+
+                if (input[i][j] == 'S')
+                {
+                    if (input[i + 1][j] == 'A')
+                    {
+                        if (input[i + 2][j] == 'M')
+                        {
+                            if (input[i + 3][j] == 'X')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < input.Count - 3; i++)
+        {
+            for (int j = 0; j < input[i].Length - 3; j++)
+            {
+                if (input[i][j] == 'X')
+                {
+                    if (input[i + 1][j + 1] == 'M')
+                    {
+                        if (input[i + 2][j + 2] == 'A')
+                        {
+                            if (input[i + 3][j + 3] == 'S')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+
+                if (input[i][j] == 'S')
+                {
+                    if (input[i + 1][j + 1] == 'A')
+                    {
+                        if (input[i + 2][j + 2] == 'M')
+                        {
+                            if (input[i + 3][j + 3] == 'X')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < input.Count - 3; i++)
+        {
+            for (int j = 3; j < input[i].Length; j++)
+            {
+                if (input[i][j] == 'X')
+                {
+                    if (input[i + 1][j - 1] == 'M')
+                    {
+                        if (input[i + 2][j - 2] == 'A')
+                        {
+                            if (input[i + 3][j - 3] == 'S')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+
+                if (input[i][j] == 'S')
+                {
+                    if (input[i + 1][j - 1] == 'A')
+                    {
+                        if (input[i + 2][j - 2] == 'M')
+                        {
+                            if (input[i + 3][j - 3] == 'X')
+                            {
+                                res++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         
         return res.ToString();
     }
@@ -37,6 +169,34 @@ public class Day04 : Day
 
         int res = 0;
         
+        for (int i = 1; i < input.Count - 1; i++)
+        {
+            for (int j = 1; j < input[i].Length - 1; j++)
+            {
+                if (input[i][j] == 'A')
+                {
+                    char topleft, topright, botleft, botright;
+                    topleft = input[i - 1][j - 1];
+                    topright = input[i - 1][j + 1];
+                    botleft = input[i + 1][j - 1];
+                    botright = input[i + 1][j + 1];
+                    
+                    if (topleft == 'M' || topleft == 'S')
+                    {
+                        if (topright == 'M' || topright == 'S')
+                        {
+                            if ((botright == 'M' || botright == 'S') && botright != topleft)
+                            {
+                                if ((botleft == 'M' || botleft == 'S') && botleft != topright)
+                                {
+                                    res++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         
         return res.ToString();
     }
