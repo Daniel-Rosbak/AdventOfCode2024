eac4715b99979b32a240937dea581dfc957d18fd
Daniel-Rosbak-Student <drosba22@student.aau.dk>
2024-12-05 09:03:44 +0100

    Day 5 solved

--- AdventOfCode2024/AdventOfCode2024/Day05.cs
+++ AdventOfCode2024/AdventOfCode2024/Day05.cs
@@ -4,7 +4,7 @@ public class Day05 : Day
 {
     public override string Run()
     {
-        return PartOne();
+        return PartTwo();
     }
     
     private string PartOne()
@@ -12,14 +12,82 @@ public class Day05 : Day
         Console.WriteLine("input: ");
         List<string> input = new List<string>();
         string next = Console.ReadLine();
-        while (next != String.Empty)
+        bool almostDone = false;
+        while (next != String.Empty || almostDone != true)
         {
+            if (next == string.Empty)
+            {
+                almostDone = true;
+            }
             input.Add(next);
             next = Console.ReadLine();
         }
 
-        int res = 0;
+        Dictionary<int, List<int>> rules = new Dictionary<int, List<int>>();
+        bool go = true;
+        while (go)
+        {
+            if (input[0] == string.Empty)
+            {
+                input.RemoveAt(0);
+                break;
+            }
+
+            int key = int.Parse(input[0].Substring(0, 2));
+            try
+            {
+                rules[key].Add(int.Parse(input[0].Substring(3, 2)));
+            }
+            catch (Exception e)
+            {
+                List<int> rule = new List<int>();
+                rule.Add(int.Parse(input[0].Substring(3, 2)));
+                rules.Add(key,rule);
+            }
+            input.RemoveAt(0);
+        }
         
+        int res = 0;
+
+        foreach (string update in input)
+        {
+            string[] stringPages = update.Split(",");
+            List<int> pages = new List<int>();
+            for (int i = 0; i < stringPages.Length; i++)
+            {
+                pages.Add(int.Parse(stringPages[i]));
+            }
+
+            bool rightOrder = true;
+            for (int i = 0; i < pages.Count; i++)
+            {
+                int currentPage = pages[i];
+                for (int j = 0; j < pages.Count; j++)
+                {
+                    try
+                    {
+                        if (rules[currentPage].Contains(pages[j]) && i > j)
+                        {
+                            rightOrder = false;
+                            break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                if (!rightOrder)
+                {
+                    break;
+                }
+            }
+
+            if (rightOrder)
+            {
+                res += pages[pages.Count / 2];
+            }
+        }
         
         return res.ToString();
     }
@@ -29,14 +97,79 @@ public class Day05 : Day
         Console.WriteLine("input: ");
         List<string> input = new List<string>();
         string next = Console.ReadLine();
-        while (next != String.Empty)
+        bool almostDone = false;
+        while (next != String.Empty || almostDone != true)
         {
+            if (next == string.Empty)
+            {
+                almostDone = true;
+            }
             input.Add(next);
             next = Console.ReadLine();
         }
 
-        int res = 0;
+        Dictionary<int, List<int>> rules = new Dictionary<int, List<int>>();
+        bool go = true;
+        while (go)
+        {
+            if (input[0] == string.Empty)
+            {
+                input.RemoveAt(0);
+                break;
+            }
+
+            int key = int.Parse(input[0].Substring(0, 2));
+            try
+            {
+                rules[key].Add(int.Parse(input[0].Substring(3, 2)));
+            }
+            catch (Exception e)
+            {
+                List<int> rule = new List<int>();
+                rule.Add(int.Parse(input[0].Substring(3, 2)));
+                rules.Add(key,rule);
+            }
+            input.RemoveAt(0);
+        }
         
+        int res = 0;
+
+        foreach (string update in input)
+        {
+            string[] stringPages = update.Split(",");
+            List<int> pages = new List<int>();
+            for (int i = 0; i < stringPages.Length; i++)
+            {
+                pages.Add(int.Parse(stringPages[i]));
+            }
+
+            bool correctedOrder = false;
+            for (int i = 0; i < pages.Count; i++)
+            {
+                int currentPage = pages[i];
+                List<int> rule;
+                bool exists = rules.TryGetValue(currentPage, out rule);
+                if (exists)
+                {
+                    for (int j = 0; j < pages.Count; j++)
+                    {
+                        if (rule.Contains(pages[j]) && i > j)
+                        {
+                            pages.Insert(i + 1, pages[j]);
+                            pages.RemoveAt(j);
+                            
+                            correctedOrder = true;
+                            i -= 1;
+                        }
+                    }
+                }
+            }
+
+            if (correctedOrder)
+            {
+                res += pages[pages.Count / 2];
+            }
+        }
         
         return res.ToString();
     }
--- AdventOfCode2024/AdventOfCode2024/DaySelector.cs
+++ AdventOfCode2024/AdventOfCode2024/DaySelector.cs
@@ -4,7 +4,7 @@ public class DaySelector
 {
     public static void Main()
     {
-        Day day = new Day04();
+        Day day = new Day05();
         Console.WriteLine(day.Run());
     }
 }
