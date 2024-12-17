7a05bcc7d9af6c69540fc92a96ee71c6e63624e1
Daniel-Rosbak-Student <drosba22@student.aau.dk>
2024-12-17 17:42:28 +0100

    completed day 17

--- AdventOfCode2024/AdventOfCode2024/Day17.cs
+++ AdventOfCode2024/AdventOfCode2024/Day17.cs
@@ -4,22 +4,77 @@ public class Day17 : Day
 {
     public override string Run()
     {
-        return PartOne();
+        return PartTwo();
     }
     
     private string PartOne()
     {
         Console.WriteLine("input: ");
-        List<string> input = new List<string>();
+        List<long> registers = new List<long>();
         string next = Console.ReadLine();
         while (next != String.Empty)
         {
-            input.Add(next);
+            registers.Add(long.Parse(next.Substring(12)));
             next = Console.ReadLine();
         }
 
-        int res = 0;
-        
+        List<int> instructions = new List<int>();
+        string[] inst = Console.ReadLine().Substring(9).Split(",");
+
+        for (int i = 0; i < inst.Length; i++)
+        {
+            instructions.Add(int.Parse(inst[i]));
+        }
+
+        string res = "";
+
+        for (int i = 0; i < instructions.Count; i += 2)
+        {
+            long literal = instructions[i + 1], combo = literal;
+            switch (literal)
+            {
+                case 4:
+                    combo = registers[0];
+                    break;
+                case 5:
+                    combo = registers[1];
+                    break;
+                case 6:
+                    combo = registers[2];
+                    break;
+            }
+            
+            
+            switch (instructions[i])
+            {
+                case 0:
+                    registers[0] /= (long)Math.Pow(2, combo);
+                    break;
+                case 1:
+                    registers[1] ^= literal;
+                    break;
+                case 2:
+                    registers[1] = combo % 8;
+                    break;
+                case 3:
+                    if (registers[0] == 0)
+                        continue;
+                    i = (int)literal - 2;
+                    break;
+                case 4:
+                    registers[1] ^= registers[2];
+                    break;
+                case 5:
+                    res += combo % 8 + ",";
+                    break;
+                case 6:
+                    registers[1] = registers[0] / (long)Math.Pow(2, combo);
+                    break;
+                case 7:
+                    registers[2] = registers[0] / (long)Math.Pow(2, combo);
+                    break;
+            }
+        }
         
         return "Answer for part 1: " + res;
     }
@@ -27,16 +82,112 @@ public class Day17 : Day
     private string PartTwo()
     {
         Console.WriteLine("input: ");
-        List<string> input = new List<string>();
+        List<long> registers = new List<long>();
         string next = Console.ReadLine();
         while (next != String.Empty)
         {
-            input.Add(next);
+            registers.Add(int.Parse(next.Substring(12)));
             next = Console.ReadLine();
         }
 
-        int res = 0;
+        List<int> instructions = new List<int>();
+        string program = Console.ReadLine().Substring(9);
+        string[] inst = program.Split(",");
+
+        for (int i = 0; i < inst.Length; i++)
+        {
+            instructions.Add(int.Parse(inst[i]));
+        }
+
+        long output = 0;
+        List<long> priorres = new List<long>();
+        long res = 0;
         
+        bool success = true;
+
+        for (int j = instructions.Count - 1; j >= 0; j--)
+        {
+            if (success)
+                res = (res * 8) - 1;
+
+            success = true;
+            
+            int current = instructions[j];
+            
+            do
+            {
+                res++;
+                registers[0] = res;
+                registers[1] = 0;
+                registers[2] = 0;
+            
+                for (int i = 0; i < instructions.Count - 2; i += 2)
+                {
+                    long literal = instructions[i + 1], combo = literal;
+                    switch (literal)
+                    {
+                        case 4:
+                            combo = registers[0];
+                            break;
+                        case 5:
+                            combo = registers[1];
+                            break;
+                        case 6:
+                            combo = registers[2];
+                            break;
+                    }
+            
+            
+                    switch (instructions[i])
+                    {
+                        case 0:
+                            registers[0] /= (long)Math.Pow(2, combo);
+                            break;
+                        case 1:
+                            registers[1] ^= literal;
+                            break;
+                        case 2:
+                            registers[1] = combo % 8;
+                            break;
+                        case 3:
+                            if (registers[0] == 0)
+                                continue;
+                            i = (int)literal - 2;
+                            break;
+                        case 4:
+                            registers[1] ^= registers[2];
+                            break;
+                        case 5:
+                            output = combo % 8;
+                            break;
+                        case 6:
+                            registers[1] = registers[0] / (long)Math.Pow(2, combo);
+                            break;
+                        case 7:
+                            registers[2] = registers[0] / (long)Math.Pow(2, combo);
+                            break;
+                    }
+                }
+
+                if (priorres.Count > 0 && res / 8 != priorres[^1])
+                {
+                    success = false;
+                    res = priorres[^1];
+                    j += 2;
+                    break;
+                }
+                
+            } while (output != current);
+
+            if (success)
+            {
+                priorres.Add(res);
+            }
+            else
+            {
+                priorres.RemoveAt(priorres.Count - 1);
+            }
+        }
         
         return "Answer for part 2: " + res;
     }
--- AdventOfCode2024/AdventOfCode2024/DaySelector.cs
+++ AdventOfCode2024/AdventOfCode2024/DaySelector.cs
@@ -4,7 +4,7 @@ public class DaySelector
 {
     public static void Main()
     {
-        Day day = new Day16();
+        Day day = new Day17();
         Console.WriteLine(day.Run());
     }
 }
