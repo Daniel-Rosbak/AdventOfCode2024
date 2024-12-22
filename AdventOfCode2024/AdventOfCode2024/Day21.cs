5ec8498ddff89923b875ded97b88748e4563a645
Daniel-Rosbak-Student <drosba22@student.aau.dk>
2024-12-22 14:20:56 +0100

    day 22 part 1 completed

--- AdventOfCode2024/AdventOfCode2024/Day21.cs
+++ AdventOfCode2024/AdventOfCode2024/Day21.cs
@@ -17,10 +17,216 @@ public class Day21 : Day
             input.Add(next);
             next = Console.ReadLine();
         }
-
+        
+        Dictionary<int, int[]> numPad = new Dictionary<int, int[]>();
+        // -1 = A
+        numPad[-1] = new[] { 2, 3 };
+        numPad[0] = new[] { 1, 3 };
+        numPad[1] = new[] { 0, 2 };
+        numPad[2] = new[] { 1, 2 };
+        numPad[3] = new[] { 2, 2 };
+        numPad[4] = new[] { 0, 1 };
+        numPad[5] = new[] { 1, 1 };
+        numPad[6] = new[] { 2, 1 };
+        numPad[7] = new[] { 0, 0 };
+        numPad[8] = new[] { 1, 0 };
+        numPad[9] = new[] { 2, 0 };
+        Dictionary<int, int[]> keyPad = new Dictionary<int, int[]>();
+        // -1 = A
+        keyPad[-1] = new[] { 2, 0 };
+        // 0 = ^
+        keyPad[0] = new[] { 1, 0 };
+        // 1 = >
+        keyPad[1] = new[] { 2, 1 };
+        // 2 = v
+        keyPad[2] = new[] { 1, 1 };
+        // 3 = <
+        keyPad[3] = new[] { 0, 1 };
+        
         int res = 0;
+
+        for (int i = 0; i < input.Count; i++)
+        {
+            int[] numPadRobot = { 2, 3 }, keyPadRobotOne = { 2, 0 }, keyPadRobotTwo = { 2, 0 };
+            List<int[]> moves = new List<int[]>();
+            List<int[]> priorMoves = new List<int[]>();
+            for (int j = 0; j < input[i].Length; j++)
+            {
+                char cNumber = input[i][j];
+                int number;
+                if (cNumber == 'A')
+                    number = -1;
+                else
+                    number = int.Parse(cNumber.ToString());
+
+                int[] nextKey = numPad[number];
+
+                moves.Add(new[] { nextKey[0] - numPadRobot[0], nextKey[1] - numPadRobot[1], number});
+
+                numPadRobot = nextKey;
+            }
+
+            //robots to control
+            priorMoves = moves;
+            moves = new List<int[]>();
+            int movingFrom = -1, movingTo;
+            for (int j = 0; j < priorMoves.Count; j++)
+            {
+                int[] move = priorMoves[j];
+                int x = move[0], y = move[1];
+                movingTo = move[2];
+                if (!((movingFrom == -1 || movingFrom == 0) && (movingTo == 1 || movingTo == 4 || movingTo == 7)))
+                {
+                    if (x != 0)
+                    {
+                        int keyX = x < 0 ? 3 : 1;
+                        moves.Add(new[] { keyPad[keyX][0] - keyPadRobotOne[0], keyPad[keyX][1] - keyPadRobotOne[1], keyX});
+                        keyPadRobotOne = keyPad[keyX];
+                        x += x < 0 ? 1 : -1;
+
+                        for (int k = x; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0, keyX});
+                        }
+                    }
+
+                    if (y != 0)
+                    {
+                        int keyY = y < 0 ? 0 : 2;
+                        moves.Add(new[] { keyPad[keyY][0] - keyPadRobotOne[0], keyPad[keyY][1] - keyPadRobotOne[1], keyY});
+                        keyPadRobotOne = keyPad[keyY];
+                        y += y < 0 ? 1 : -1;
+
+                        for (int k = y; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0, keyY});
+                        }
+                    }
+                }
+                else
+                {
+                    if (y != 0)
+                    {
+                        int keyY = y < 0 ? 0 : 2;
+                        moves.Add(new[] { keyPad[keyY][0] - keyPadRobotOne[0], keyPad[keyY][1] - keyPadRobotOne[1], keyY});
+                        keyPadRobotOne = keyPad[keyY];
+                        y += y < 0 ? 1 : -1;
+
+                        for (int k = y; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0, keyY});
+                        }
+                    }
+
+                    if (x != 0)
+                    {
+                        int keyX = x < 0 ? 3 : 1;
+                        moves.Add(new[] { keyPad[keyX][0] - keyPadRobotOne[0], keyPad[keyX][1] - keyPadRobotOne[1], keyX});
+                        keyPadRobotOne = keyPad[keyX];
+                        x += x < 0 ? 1 : -1;
+
+                        for (int k = x; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0, keyX});
+                        }
+                    }
+                }
+
+                moves.Add(new[] { keyPad[-1][0] - keyPadRobotOne[0], keyPad[-1][1] - keyPadRobotOne[1], -1 });
+                keyPadRobotOne = keyPad[-1];
+
+                movingFrom = movingTo;
+            }
+
+            priorMoves = moves;
+            moves = new List<int[]>();
+            movingFrom = -1;
+            for (int j = 0; j < priorMoves.Count; j++)
+            {
+                int[] move = priorMoves[j];
+                int x = move[0], y = move[1];
+
+                movingTo = move[2];
+                if (!((movingFrom == -1 || movingFrom == 0) && movingTo == 3))
+                {
+                    if (x != 0)
+                    {
+                        int keyX = x < 0 ? 3 : 1;
+                        moves.Add(new[] { keyPad[keyX][0] - keyPadRobotOne[0], keyPad[keyX][1] - keyPadRobotOne[1] });
+                        keyPadRobotOne = keyPad[keyX];
+                        x += x < 0 ? 1 : -1;
+
+                        for (int k = x; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0 });
+                        }
+                    }
+
+                    if (y != 0)
+                    {
+                        int keyY = y < 0 ? 0 : 2;
+                        moves.Add(new[] { keyPad[keyY][0] - keyPadRobotOne[0], keyPad[keyY][1] - keyPadRobotOne[1] });
+                        keyPadRobotOne = keyPad[keyY];
+                        y += y < 0 ? 1 : -1;
+
+                        for (int k = y; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0 });
+                        }
+                    }
+                }
+                else
+                {
+                    if (y != 0)
+                    {
+                        int keyY = y < 0 ? 0 : 2;
+                        moves.Add(new[] { keyPad[keyY][0] - keyPadRobotOne[0], keyPad[keyY][1] - keyPadRobotOne[1] });
+                        keyPadRobotOne = keyPad[keyY];
+                        y += y < 0 ? 1 : -1;
+
+                        for (int k = y; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0 });
+                        }
+                    }
+
+                    if (x != 0)
+                    {
+                        int keyX = x < 0 ? 3 : 1;
+                        moves.Add(new[] { keyPad[keyX][0] - keyPadRobotOne[0], keyPad[keyX][1] - keyPadRobotOne[1] });
+                        keyPadRobotOne = keyPad[keyX];
+                        x += x < 0 ? 1 : -1;
+
+                        for (int k = x; k != 0; k += k < 0 ? 1 : -1)
+                        {
+                            moves.Add(new[] { 0, 0 });
+                        }
+                    }
+                }
+
+                moves.Add(new[] { keyPad[-1][0] - keyPadRobotOne[0], keyPad[-1][1] - keyPadRobotOne[1] });
+                keyPadRobotOne = keyPad[-1];
+
+                movingFrom = movingTo;
+            }
+
+            int sequenceLength = 0;
+
+            Console.WriteLine();
+            for (int j = 0; j < moves.Count; j++)
+            {
+                int[] move = moves[j];
+                Console.Write(move[0] + "" + move[1] + "A");
+                sequenceLength++;
+                sequenceLength += Math.Abs(move[0]);
+                sequenceLength += Math.Abs(move[1]);
+            }
+
+            res += sequenceLength * int.Parse(input[i].Remove(3));
+            Console.WriteLine(sequenceLength + " " + input[i].Remove(3));
+        }
         
-        
+        //94758 too high
         return "Answer for part 1: " + res;
     }
 
@@ -36,8 +242,7 @@ public class Day21 : Day
         }
 
         int res = 0;
-        
-        
+
         return "Answer for part 2: " + res;
     }
 }
--- AdventOfCode2024/AdventOfCode2024/Day22.cs
+++ AdventOfCode2024/AdventOfCode2024/Day22.cs
@@ -4,7 +4,7 @@ public class Day22 : Day
 {
     public override string Run()
     {
-        return PartOne();
+        return PartTwo();
     }
     
     private string PartOne()
@@ -18,10 +18,30 @@ public class Day22 : Day
             next = Console.ReadLine();
         }
 
-        int res = 0;
+        long res = 0;
+        
+        for (int i = 0; i < input.Count; i++)
+        {
+            res += Find2000ThNumber(long.Parse(input[i]));
+        }
         
         
         return "Answer for part 1: " + res;
+    }
+
+    private long Find2000ThNumber(long startingNumber)
+    {
+        for (int i = 0; i < 2000; i++)
+        {
+            startingNumber ^= startingNumber * 64;
+            startingNumber %= 16777216;
+            startingNumber ^= startingNumber / 32;
+            startingNumber %= 16777216;
+            startingNumber ^= startingNumber * 2048;
+            startingNumber %= 16777216;
+        }
+        
+        return startingNumber;
     }
 
     private string PartTwo()
@@ -35,9 +55,77 @@ public class Day22 : Day
             next = Console.ReadLine();
         }
 
+        List<Dictionary<string, int>> changesPerSecret = new List<Dictionary<string, int>>();
+        for (int i = 0; i < input.Count; i++)
+        {
+            changesPerSecret.Add(GetChanges(long.Parse(input[i])));
+        }
+
         int res = 0;
-        
+        for (int i = 0; i < changesPerSecret.Count; i++)
+        {
+            foreach (KeyValuePair<string, int> pair in changesPerSecret[i])
+            {
+                int potentialProfit = 0;
+                string pattern = pair.Key;
+                for (int j = 0; j < changesPerSecret.Count; j++)
+                {
+                    if (changesPerSecret[j].TryGetValue(pattern, out int price))
+                    {
+                        potentialProfit += price;
+                    }
+                }
+
+                if (pattern == "-2 1 -1 3")
+                {
+                    if (changesPerSecret[i].TryGetValue(pattern, out int p))
+                    {
+                        Console.WriteLine(i + "" + p);
+                    }
+                }
+
+                if (potentialProfit >= res)
+                {
+                    res = potentialProfit;
+                }
+            }
+        }
         
         return "Answer for part 2: " + res;
+    }
+    
+    private Dictionary<string, int> GetChanges(long startingNumber)
+    {
+        Dictionary<string, int> marks = new Dictionary<string, int>();
+        int[] changes = new int [2001];
+        int prior = (int)(startingNumber%10);
+        changes[0] = prior;
+        
+        for (int i = 0; i < 2000; i++)
+        {
+            startingNumber ^= startingNumber * 64;
+            startingNumber %= 16777216;
+            startingNumber ^= startingNumber / 32;
+            startingNumber %= 16777216;
+            startingNumber ^= startingNumber * 2048;
+            startingNumber %= 16777216;
+            changes[i + 1] = (int)(startingNumber % 10) - prior;
+            prior = (int)(startingNumber % 10);
+        }
+
+        int price = changes[0] + changes[1] + changes[2] + changes[3];
+
+        for (int i = 4; i < changes.Length; i++)
+        {
+            price += changes[i];
+            string here = changes[i - 3] + " " + changes[i - 2] + " " + changes[i - 1] + " " + changes[i];
+            if (marks.ContainsKey(here))
+            {
+                break;
+            }
+            marks[here] = price;
+        }
+        
+        return marks;
     }
 }
